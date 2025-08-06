5478544a5746157bc4484c9f470b8b0f24a71c3b
Darius Deng <dariusdengyang@gmail.com>
2025-08-06 09:42:57 +0800

    Initial Commit

---
+++ CerebroTests/CerebroTests.swift
@@ -0,0 +1,17 @@
+//
+//  CerebroTests.swift
+//  CerebroTests
+//
+//  Created by Darius Deng on 6/8/25.
+//
+
+import Testing
+@testable import Cerebro
+
+struct CerebroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
